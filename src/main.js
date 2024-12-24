ce6fca47a8f167d76973a91513b91722217a8e54
Zhihao Yang <zhihaoyang@wustl.edu>
2024-12-24 13:05:13 -0600

    fixed path

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/global.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // 引入 Bootstrap 样式
+import './assets/global.css'; // 自定义全局样式
 
 const app = createApp(App);
 app.use(router); // 注册路由
-app.mount('#app');
+app.mount('#app'); // 挂载应用
