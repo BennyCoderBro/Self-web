f4cc2a30f0a598acfaf44c6f4489d2055b838a71
Zhihao Yang <zhihaoyang@wustl.edu>
2024-12-24 00:52:59 -0600

    Add Vue files and configuration

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     extensions: ['.js', '.json', '.vue'], // 添加 .vue 扩展名解析支持
-  }
+  },
+  base: '/Self-web/' // 设置 publicPath 为 /Self-web/
 });
