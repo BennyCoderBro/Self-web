e1f7e740eb8f9b91b405e8dab08069b8361a6829
Zhihao Yang <zhihaoyang@wustl.edu>
2024-12-25 22:43:26 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import vue from '@vitejs/plugin-vue'; // 已采用静态导入，无需修改
+
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     extensions: ['.js', '.json', '.vue','.css'], // 添加 .vue 扩展名解析支持
-  },
-  base: './'
+  }
 });
