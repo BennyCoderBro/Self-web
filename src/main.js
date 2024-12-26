6c76c25f739e1e9ca1660e53852350f87bd5291f
Zhihao Yang <zhihaoyang@wustl.edu>
2024-12-25 23:28:52 -0600

    test @moudle in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp } from '/@modules/vue';
 import App from './App.vue';
 import router from './router.js';
 import 'bootstrap/dist/css/bootstrap.min.css'; // 引入 Bootstrap 样式
